9607ea1242730ecfc4c3c067a6f61b2398b6a732
hask <hask256@gmail.com>
2012-07-08 23:09:31 +0200

    sudoku find_possible

--- sudoku.hs
+++ sudoku.hs
@@ -34,10 +34,23 @@ get_square ms i j = foldl1 (++) $ map (take 3 . drop (j*3)) rs
 get_all_squares ms = [(get_square ms i j) | i <- [0..2] , j <- [0..2]]
 
 
+find_possible ms i j = [x | x <- [1..9], x `notElem` filled]
+
+
+    where
+    row = ms !! i
+    col = (transpose ms) !! j
+    square = get_square ms (i `div` 3) (j `div` 3)
+    filled = filter (>0) $ nub $ row ++ col ++ square
+
+
 main = do input <- readFile "input.txt"
           let xs = map _int $ filter ((0<).length) $ split "\n\r " $ input
           let ms = make_list xs 9
           let ch = check ms
-          putStrLn $ show ch
-          putStrLn $ show ms
+          let fu = find_possible ms 0 0
+          let all_possible = [(find_possible ms i j, i, j, (ms !! i) !! j) | i <- [0..8], j <- [0..8]]
+          mapM print all_possible
+          putStrLn $ show fu
+          mapM print ms
 
