4ab13414816b63f388279fcfeb95dfd78e7bbd24
hask <hask256@gmail.com>
2012-07-08 23:28:04 +0200

    sudoku find_possible improve

--- sudoku.hs
+++ sudoku.hs
@@ -26,7 +26,6 @@ check ms = (allT $ map check9 ms) &&
            (allT $ map check9 $ get_all_squares ms)
 
 get_square ms i j = foldl1 (++) $ map (take 3 . drop (j*3)) rs
-
     where
     rs = take 3 . drop (i*3) $ ms
    
@@ -35,13 +34,20 @@ get_all_squares ms = [(get_square ms i j) | i <- [0..2] , j <- [0..2]]
 
 
 find_possible ms i j = [x | x <- [1..9], x `notElem` filled]
-
-
     where
     row = ms !! i
     col = (transpose ms) !! j
     square = get_square ms (i `div` 3) (j `div` 3)
     filled = filter (>0) $ nub $ row ++ col ++ square
+
+list_replace xs n el = take n xs ++ [el] ++ drop (n + 1) xs
+
+board_replace ms i j el = take i ms ++ [new_row] ++ drop (i + 1) ms 
+    where
+    row = ms !! i
+    new_row = list_replace row j el
+
+at xs i j = (xs !! i) !! j
 
 
 main = do input <- readFile "input.txt"
@@ -49,8 +55,12 @@ main = do input <- readFile "input.txt"
           let ms = make_list xs 9
           let ch = check ms
           let fu = find_possible ms 0 0
-          let all_possible = [(find_possible ms i j, i, j, (ms !! i) !! j) | i <- [0..8], j <- [0..8]]
+          let all_possible = [(find_possible ms i j, (i, j)) | i <- [0..8], j <- [0..8], at ms i j == 0]
           mapM print all_possible
           putStrLn $ show fu
+          let new_b = board_replace ms 1 1 9
+            
           mapM print ms
+          putStrLn ""
+          mapM print new_b
 
